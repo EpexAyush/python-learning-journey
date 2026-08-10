5e79a0ac81c9529f3136798d492cf266a044da22
EpexAyush <epexayush@gmail.com>
2026-08-11 00:08:33 +0530

    Update keywords.py

---
+++ module 1/keywords.py
@@ -0,0 +1,12 @@
+# Keyword Auditor
+import keyword
+print("Welcome to the system keyword auditor.")
+print(f"Total keywords in the python are : {len(keyword.kwlist)}")
+print(f"These are the list of keywords in the python: {keyword.kwlist}")
+first_kw= "while"
+second_kw="if"
+third_kw="elif"
+print(f"""these are the keywords: 
+1:{first_kw}
+2:{second_kw}
+3:{third_kw}""")
