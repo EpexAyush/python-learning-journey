8787031fe8a6c1ec2460744460ba1248315b4e0c
EpexAyush <epexayush@gmail.com>
2026-08-13 08:01:41 +0530

    Create primitive_datatypes.py

---
+++ module 1/primitive_datatypes.py
@@ -0,0 +1,45 @@
+#====================================
+# Section A: Identify The Data Type
+#====================================
+
+score=95
+temperature=-2.5
+is_active=True
+phone_number="9876543210"
+price=0.0
+
+#output: in sabhi variables me ye datatype stored hai
+print(type(score)) # output: Int
+print(type(temperature)) #output: float
+print(type(is_active)) #output: Boolean
+print(type(phone_number)) #output: String
+print(type(price)) #output: float
+
+
+#=====================================
+#Section B: Spot the Error
+#=====================================
+
+# is_valid= true     
+# my_fraction=3,14
+# greeting='Hello world"
+
+# datatype me correction 
+is_valid= True #booleans ka first letter capital hota hai 
+my_fraction=3.14 #comma is replaced by the dot
+greeting="Hello World" #use same type of quotes on both side of a string
+
+
+#======================================
+# Section C: Practical Coding Task
+#======================================\
+
+#step 1: variables ko unka data type assign
+user_name="Ayush Kumar"
+age=21
+is_student=True
+
+#step 2: variable ke andr kis type ka datatype store hai use print karna
+print(type(user_name))
+print(type(age))
+print(type(is_student))
