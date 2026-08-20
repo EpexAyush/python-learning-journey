86cbf93ebc0aa310a8a0ffecc76f88831638a955
EpexAyush <epexayush@gmail.com>
2026-08-20 15:45:12 +0530

    Create grade_calculator.py

---
+++ grade_calculator.py
@@ -0,0 +1,10 @@
+stu_marks=int(input("Enter Your Marks: "))
+
+if stu_marks>=90 and stu_marks<=100:
+    print("Grade A")
+elif stu_marks>=80 and stu_marks<90:
+    print("Grade B")
+elif stu_marks>=70 and stu_marks<80:
+    print("Grade C")
+else:
+    print("Fail")
