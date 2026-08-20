0faf4e1d8bac1802dc4bfb9bde38554662675ee1
EpexAyush <epexayush@gmail.com>
2026-08-20 15:27:39 +0530

    Create Even or Odd Number Checker.py

---
+++ Even or Odd Number Checker.py
@@ -0,0 +1,12 @@
+# =============================
+# Even or Odd Number Checker
+# =============================
+
+#Taking an input from user as a integar
+num=int(input("Enter a number: "))
+
+#conditional block
+if num%2==0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
